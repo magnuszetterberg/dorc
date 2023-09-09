87a10a6ca406d90047f2253f52ef8efdb0ae1fdb
magnus zetterberg <magnus.zetterberg@combitech.com>
2023-09-09 12:39:37 +0200

    republish to mqtt

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
 import paho.mqtt.client as mqtt
 import os
 from dotenv import load_dotenv
-import uuid
-import re
 import subprocess
+import paho.mqtt.publish as publish
+import json
 
-# SETUP ENV 
+# SETUP ENV
 # Load environment variables from .env file
 load_dotenv()
+
 # MQTT settings
 MQTT_BROKER_HOST = os.getenv("MQTT_BROKER_HOST")
 MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT"))
 MQTT_TOPIC = os.getenv("MQTT_TOPIC")
+RESPONSE_TOPIC = os.getenv("RESPONSE_TOPIC")
+RESPONSE_PAYLOAD = os.getenv("RESPONSE_PAYLOAD")
+
 # Docker settings
 DOCKER_CONTAINER_COMMAND = os.getenv("DOCKER_CONTAINER_COMMAND")
 DOCKER_CONTAINER_NAME_PREFIX = os.getenv("DOCKER_CONTAINER_NAME_PREFIX")
 
-# Function to generate a unique container name
-# def generate_container_name():
-#     random_str = str(uuid.uuid4())[:8]
-#     # Remove any characters that are not allowed in container names
-#     sanitized_str = re.sub(r'[^a-z0-9_]', '_', random_str)
-#     return f"{DOCKER_CONTAINER_NAME_PREFIX}-{sanitized_str}"
-
 # Function to start a Docker container with a given payload
 def start_container(payload):
-    # container_name = generate_container_name()
     try:
-        # Run the Docker command
-        subprocess.run(DOCKER_CONTAINER_COMMAND, shell=True)
+        subprocess.run([DOCKER_CONTAINER_COMMAND], shell=True)
+        print(f"Container started with payload: {payload}")
+
+        # Create a dictionary for the response payload
+        response_payload = {
+            "active": RESPONSE_PAYLOAD
+        }
+
+        # Convert the dictionary to a JSON string
+        response_json = json.dumps(response_payload)
+
+        # Publish the JSON message back to the specified topic
+        publish.single(RESPONSE_TOPIC, response_json, hostname=MQTT_BROKER_HOST, port=MQTT_BROKER_PORT)
     except subprocess.CalledProcessError as e:
         print(f"Error starting container: {e}")
-
-
-
 
 # MQTT on_connect callback
 def on_connect(client, userdata, flags, rc):
